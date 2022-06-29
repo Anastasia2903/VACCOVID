d0fdb2e1e511ae9316e34ff30c71ee073d2d0c45
Anastasia2903 <kudinova342@gmail.com>
2022-06-29 19:50:04 +0300

    Добавьте файлы проекта.

---
+++ VACCOVID/CovidNewsParameter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VACCOVID
+{
+   public class CovidNewsParameter
+   {
+        public string Page { get; set; }
+   }
+}
---
+++ VACCOVID/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
+using Telegram.Bot.Extensions.Polling;
+
+namespace VACCOVID
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VACCOVID VACCOVID = new VACCOVID();
+            VACCOVID.Start();
+            Console.ReadKey();
+
+
+        }
+    }
+}
